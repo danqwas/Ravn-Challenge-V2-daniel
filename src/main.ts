ff54a2399ee61a2ec53d783b0a6293b474b994cb
Tu nombre <daniel_inmaculada@hotmail.com>
2023-12-11 23:01:18 -0500

    core: Add configuration settings
    Implement Swagger, CORS, Versioning, global Pipes, Cookie Parser, Class Validator, Class Transformer, and ConfigModule integrations in the project.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,41 @@
+import * as cookieParser from 'cookie-parser';
+
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // Enable CORS
+  app.enableCors({
+    origin: [],
+  });
+  // Enable versioning
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+  // Enable validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  // Enable cookie parser
+  app.use(cookieParser());
+  // Swagger Setup
+  const options = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API documentation for your application')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
