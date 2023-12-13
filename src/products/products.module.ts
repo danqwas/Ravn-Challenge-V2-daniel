f7f60c8ed5fe68409632109bc366fa9d761a2a37
Tu nombre <daniel_inmaculada@hotmail.com>
2023-12-13 01:15:59 -0500

    core(products): import AuthModule
    Add AuthModule to the products Module.

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,12 @@
+import { AuthModule } from 'src/auth/auth.module';
+
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
+
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
