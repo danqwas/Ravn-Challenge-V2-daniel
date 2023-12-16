e752ae03f64331c2d582fc16c42267ea52517ffd
Tu nombre <daniel_inmaculada@hotmail.com>
2023-12-16 11:22:25 -0500

    refactor(auth): update authentication controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,7 +13,7 @@ import { Auth } from './decorators/auth.decorator';
 import { GetUser } from './decorators/get-user.decorator';
 import { CreateUserDto, LoginUserDto, LogoutUserDto } from './dto';
 
-@ApiTags('Auth Controller')
+@ApiTags('Authentication')
 @Controller({ path: 'auth', version: '1' })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
